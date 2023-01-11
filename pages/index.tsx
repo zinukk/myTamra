6e9f257198bf4c89a16160026a9c9d8a87ee500d
zinukk <slsxpseh54@naver.com>
2023-01-11 15:14:44 +0900

    ADD : 홈화면 페이지 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,31 @@
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
+import MainBg from '../public/assets/images/image_bg.gif';
+
+interface {
+  MainBg : 
+}
+
 const Home = () => {
-  return <div>홈화면</div>;
+  console.log(MainBg);
+
+  return <StHome>홈화면</StHome>;
 };
+
+const StHome = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-image: url(${MainBg});
+  background-size: cover;
+  background-repeat: no-repeat;
+  cursor: pointer;
+`;
+
+// const StHome = styled.div`
+//   position: relative;
+//   width: 100%;
+//   height: 100%;
+// `;
 
 export default Home;
