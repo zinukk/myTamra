64b2d48ca509cec4770b4a9e2b3adc67edea3a1f
zinukk <slsxpseh54@naver.com>
2023-01-11 16:00:27 +0900

    ADD : theme 추가

---
+++ src/components/common/Layout/style.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const LayoutWrapStyle = styled.div`
+  margin: 0 auto;
+  max-width: 430px;
+  height: 100vh;
+  border: 1px solid black;
+`;
---
+++ src/emotion.d.ts
@@ -0,0 +1,10 @@
+import '@emotion/react';
+import { theme } from '../styles/theme';
+
+type ThemeType = typeof theme;
+
+declare module '@emotion/react' {
+  export interface Theme extends ThemeType {
+    color: Color;
+  }
+}
