40437021a939b181c46d7969f3460ba7a59ca6f0
zinukk <slsxpseh54@naver.com>
2023-01-11 16:12:58 +0900

    ADD : Layout 컴포넌트 구현 후 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 import global from 'styles/global';
 import theme from 'styles/theme';
+import Layout from '@src/components/common/Layout/Layout';
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [queryClient] = useState(() => new QueryClient());
@@ -13,8 +14,10 @@ const App = ({ Component, pageProps }: AppProps) => {
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <ThemeProvider theme={theme}>
-          <Global styles={global} />
-          <Component {...pageProps} />
+          <Layout>
+            <Global styles={global} />
+            <Component {...pageProps} />
+          </Layout>
         </ThemeProvider>
       </RecoilRoot>
     </QueryClientProvider>
