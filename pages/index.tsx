ea42314044838a3acbd8aef0b536c7f9d15ec7a0
zinukk <slsxpseh54@naver.com>
2023-02-05 16:54:51 +0900

    Fix : ci/cd 에러

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const Home = () => {
         }}>
         <StHeader>{/* <Image src={MainLogo} width={300} height={150} alt="로고" /> */}</StHeader>
         {/* <Snowfall snowflakeCount={60} /> */}
-        {/* <StFooter MainText={MainText} /> */}
+        <StFooter MainText={MainText} />
       </StHome>
     </>
   );
