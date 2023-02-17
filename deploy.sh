1a3472a631f0657fb9597d8e0c27bd9ac4966b81
zinukk <slsxpseh54@naver.com>
2023-02-17 19:28:12 +0900

    Test : ci/cd test

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,6 @@ REPOSITORY=/home/ubuntu/deploy
 
 cd $REPOSITORY 
 
-sudo pm2 kill
-
 sudo yarn install
 
-sudo yarn build
-
-sudo pm2 start yarn --name "myTamra" --interpreter bash -- start
+sudo pm2 reload all
